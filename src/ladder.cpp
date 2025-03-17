20e4de204524bfc9a9f9f44bf68f9c3841606658
johnh20 <johnh20@uci.edu>
2025-03-17 05:35:18 +0000

    change ladder  output

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -86,6 +86,10 @@ void load_words(set<string> &word_list, const string &file_name) {
   }
 }
 void print_word_ladder(const vector<string> &ladder) {
+	if (ladder.empty()){
+		cout << "No word ladder found." << endl;
+		return;
+	}
   cout << "Word ladder found: ";
 	for (string word : ladder)
     cout << word << " ";
